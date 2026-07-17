d7bccd281d6a9695bd68bd65c5dac33de18799b0
Meghana Kuruba <kmeghana2204@gmail.com>
2026-07-17 18:14:44 +0530

    feat(arrays): Added Product of Array Except Self solution using prefix and suffix products

--- Arrays/logic/ProductOfArrayExceptSelf.java
+++ Arrays/logic/ProductOfArrayExceptSelf.java
@@ -3,17 +3,39 @@ package Arrays.logic;
 public class ProductOfArrayExceptSelf {
     public static int[] product(int[] arr){
         // Brute force approach
-        int n = arr.length;
-        int[] ans = new int[n];
 
-        for (int i=0; i<n; i++){
-            int product = 1;
-            for (int j=0; j<n; j++){
-                if(i!=j){
-                    product *= arr[j];
-                }
-            }
-            ans[i] = product;
+//        int n = arr.length;
+//        int[] ans = new int[n];
+//
+//        for (int i=0; i<n; i++){
+//            ans[i] = 1;
+//            for (int j=0; j<n; j++){
+//                if(i!=j){
+//                    ans[i] *= arr[j];
+//                }
+//            }
+//        }
+//        return ans;
+
+        // Optimal solution using prefix and suffix
+
+        int n = arr.length;
+
+        int[] left = new int[n];
+        left[0] = 1;
+        for(int i = 1; i < n; i++){
+            left[i] = left[i-1]*arr[i-1];
+        }
+
+        int[] right = new int[n];
+        right[n-1] = 1;
+        for(int i = n-2; i>=0;i--){
+            right[i] = right[i+1]*arr[i+1];
+        }
+
+        int[] ans = new int[n];
+        for(int i = 0; i<arr.length; i++){
+            ans[i] = left[i]*right[i];
         }
         return ans;
     }
